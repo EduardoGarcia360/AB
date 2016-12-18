3027f102790c93948c35b904cb7b4bbd627449f3
Eduardo <EduardoGarcia360@users.noreply.github.com>
2016-12-18 16:12:35 -0600

    arbol b c++

--- pagina.h
+++ pagina.h
@@ -1,20 +1,26 @@
 #pragma once
 #include <stdlib.h>
+#include "Clases/listadc.h"
 #define m 5
 
 using namespace std;
 
 /* Creado a partir del libro:
-	<<Algoritmos y estructuras de datos una perspectiva en C>>
-	de los autores:
-	<<Luis Joyanes Aguilar & Ignacio Zahonero Matínez>>
-	utilizando:
-	<<Visual Studio Express 2013>>
+<<Algoritmos y estructuras de datos una perspectiva en C>>
+de los autores:
+<<Luis Joyanes Aguilar & Ignacio Zahonero Matínez>>
+utilizando:
+<<Visual Studio Express 2013>>
 */
 
 class pagina{
 public:
 	int claves[m];
+	char* fecha[m];
+	char* periodo[m];
+	long int dpi[m];
+	char* placa[m];
+	listadc* Lista;
 	pagina* ramas[m];
 	int cuenta;
 	bool nodoLLeno(pagina* actual){
